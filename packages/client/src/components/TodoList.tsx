d4572f9de9ca5b46bd3a1f51b09dde21880e9a40
Dennis Morello <dennismorello@gmail.com>
2023-10-07 19:22:48 +0200

    feat: animate todo list

--- packages/client/src/components/TodoList.tsx
+++ packages/client/src/components/TodoList.tsx
@@ -1,14 +1,17 @@
 import { Todo } from "@app/openapi/todos";
+import { LayoutGroup, motion } from "framer-motion";
 import { TodoItem } from "./TodoItem";
 
 export const TodoList: React.FC<{ todos: Todo[] }> = ({ todos: posts }) => {
   return (
     <ul>
-      {posts.map((todo) => (
-        <li key={todo.id}>
-          <TodoItem todo={todo} />
-        </li>
-      ))}
+      <LayoutGroup>
+        {posts.map((todo) => (
+          <motion.li key={todo.id} layout>
+            <TodoItem todo={todo} />
+          </motion.li>
+        ))}
+      </LayoutGroup>
     </ul>
   );
 };
