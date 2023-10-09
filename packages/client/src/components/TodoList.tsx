500a9855cbfcb5fd8f2001e36489f1e933288310
Dennis Morello <dennismorello@gmail.com>
2023-10-09 08:41:14 +0200

    refactor: remove leftover references

--- packages/client/src/components/TodoList.tsx
+++ packages/client/src/components/TodoList.tsx
@@ -2,11 +2,11 @@ import { Todo } from "@app/openapi/todos";
 import { LayoutGroup, motion } from "framer-motion";
 import { TodoItem } from "./TodoItem";
 
-export const TodoList: React.FC<{ todos: Todo[] }> = ({ todos: posts }) => {
+export const TodoList: React.FC<{ todos: Todo[] }> = ({ todos }) => {
   return (
     <ul>
       <LayoutGroup>
-        {posts.map((todo) => (
+        {todos.map((todo) => (
           <motion.li key={todo.id} layout>
             <TodoItem todo={todo} />
           </motion.li>
