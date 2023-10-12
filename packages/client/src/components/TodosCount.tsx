12fb39a2f82df5136aa46dc351a4e8d2aea4cccf
Dennis Morello <dennismorello@gmail.com>
2023-10-12 09:00:01 +0200

    fix: use aria role status for todos count

--- packages/client/src/components/TodosCount.tsx
+++ packages/client/src/components/TodosCount.tsx
@@ -1,22 +1,16 @@
 import { useGetTodos } from "@app/openapi/todos";
 import { match } from "ts-pattern";
-import { getTodoHtmlId } from "../utils/todos";
+import { getCompletedTodosCount } from "../utils/todos";
 
 export const TodosCount: React.FC = () => {
   const todos = useGetTodos();
 
   return match(todos)
-    .with({ status: "success" }, ({ data }) => {
-      const todoIds = data.map(getTodoHtmlId).join(" ");
-      const completedTodos = data.filter((todo) => todo.completed);
-
-      return (
-        <div className="text-sm text-gray-500">
-          <output htmlFor={todoIds}>
-            {completedTodos.length} of {data.length} tasks done
-          </output>
-        </div>
-      );
-    })
+    .with({ status: "success" }, ({ data }) => (
+      <div role="status" className="text-sm text-gray-500">
+        <span className="tabular-nums">{getCompletedTodosCount(data)}</span> of{" "}
+        <span className="tabular-nums">{data.length}</span> tasks done
+      </div>
+    ))
     .otherwise(() => null);
 };
--- packages/client/src/utils/todos.ts
+++ packages/client/src/utils/todos.ts
@@ -5,3 +5,7 @@ const TODO_HTML_ID_PREFIX = "todo-";
 export function getTodoHtmlId(todo: Todo) {
   return `${TODO_HTML_ID_PREFIX}${todo.id}` as const;
 }
+
+export function getCompletedTodosCount(todos: Todo[]) {
+  return todos.filter((todo) => todo.completed).length;
+}
