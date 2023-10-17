25ebd74ccb80aecfba048af425b7fc6c4b47670c
Dennis Morello <dennismorello@gmail.com>
2023-10-17 14:15:31 +0200

    refactor: change random int implementation

--- packages/core/src/random.ts
+++ packages/core/src/random.ts
@@ -2,13 +2,6 @@ export function boolean() {
   return Math.random() > 0.5;
 }
 
-export function integer(
-  min: number = Number.MIN_SAFE_INTEGER,
-  max: number = Number.MAX_SAFE_INTEGER
-) {
-  if (min > max) {
-    throw new Error(`Min (${min}) cannot be greater than max (${max})`);
-  }
-
-  return Math.floor(Math.random() * (max - min + 1) + min);
+export function integer() {
+  return crypto.getRandomValues(new Uint32Array(1))[0];
 }
